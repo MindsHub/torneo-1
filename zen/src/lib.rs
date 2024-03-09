88bd7c3144229dec160f61215c11dd2707b08130
mindshub <info@mindshub.com>
2024-03-09 17:04:24 +0100

    aggiungo bot

--- zen/src/lib.rs
+++ zen/src/lib.rs
@@ -1,41 +1,36 @@
+use rand::rngs::ThreadRng;
 use template_torneo::LogTradimento;
 
 /// Scegliete quando incolpare il vostro avversario e quando no. Che vinca il migliore!!
-pub fn devo_incolparlo(me: &LogTradimento, other: &LogTradimento) -> bool {
+pub fn devo_incolparlo(me: &LogTradimento, other: &LogTradimento, rng: &mut ThreadRng) -> bool {
     if other.numero_totali() < 10 {
         //determinazione risposte avversario
-        return true;
-    } else if other.numero_totali() < 20 {
         return false;
+    } else if other.numero_totali() < 20 {
+        return true;
     }
     if other.azioni_passate()[0..10] == other.azioni_passate()[10..20] {
         //algoritmo pattern pari o univoco
-        for i in 0..10 {
-            if let Some(true) = other.azioni_passate().get(i) {
-                //se è univoca in true ritorna true
-                return false;
-            }
+        if other.azioni_passate()[0..10].iter().all(|elemento| *elemento == false){
+            return true;
         }
-        if other.azioni_passate()[0..10]
-            .iter()
-            .all(|elemento| *elemento == false)
-        {
-            return false;
+        if other.azioni_passate()[0..10].iter().all(|elemento| *elemento == true){
+            return true;
         }
-        if other.azioni_passate()[0..10]
-            .chunks(2)
-            .map(|elemento| elemento[1])
-            .all(|elemento| elemento == false)
-        {
-            return false;
+        if other.azioni_passate()[0..10].chunks(2).map(|elemento| elemento[1]).all(|elemento| elemento == false){
+            return true;
         }
-        if other.azioni_passate()[0..10]
-            .chunks(2)
-            .map(|elemento| elemento[1])
-            .all(|elemento| elemento == true)
-        {
+        if other.azioni_passate()[0..10].chunks(2).map(|elemento| elemento[1]).all(|elemento| elemento == true){
+            return true;
+        }else{
+            return true;
+        }
+    }else{
+        if let Some(false) = other.azioni_passate().last() {
             return false;
+        }else{
+            return true;
         }
     }
-    return false;
+    
 }
